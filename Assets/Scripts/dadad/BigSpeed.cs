5ff65676bacfde73addbb059511c2d822656b907
Yaren Çetinkaya <20050151001@ybu.edu.tr>
2023-05-26 20:30:45 +0300

    Speed prefabs

---
+++ Assets/Scripts/dadad/BigSpeed.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigSpeed : MonoBehaviour
+{
+    public float speed;
+    
+    //Wall collides with car ("other")
+    public void OnCollisionEnter(Collision other)
+    {
+        //arabanın hızı = arabanın hızı * speed;
+            
+    }
+}
