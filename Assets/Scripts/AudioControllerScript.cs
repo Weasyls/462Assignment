ea3e888d3c394eb9cc50090971466fe3c79f1b15
Emre Özçatal <emreozcatal@gmail.com>
2023-05-28 17:16:41 +0300

    Sesleri ekledim
    .

---
+++ Assets/Scripts/AudioControllerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControllerScript : MonoBehaviour
+{
+    public AudioSource[] musicSource;
+    public static AudioControllerScript instance;
+    public AudioControllerScript audio;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < musicSource.Length; i++) 
+        {
+            musicSource[i].Stop();
+        }
+    }
+
+    public void PlaySound(int musicToPlay)
+    {
+        musicSource[musicToPlay].Stop();
+        musicSource[musicToPlay].Play();
+    }
+
+}
