df081119255cec0c2e343465ae83fd4884966749
Emirhan Akıtürk <emirhanakiturk@gmail.com>
2023-05-30 18:13:05 +0300

    Canı ne isterse onu veren score

--- Assets/Scripts/EndZone.cs
+++ Assets/Scripts/EndZone.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class EndZone : MonoBehaviour
 {
     public Transform car;
+    public Transform point5X;
     public Transform point10X;
+    public Transform point25X;
     public Transform point50X;
+    public Transform point100X;
     public Transform point200X;
-    public Transform point500X;
-    public Transform point700X;
-    public Transform point1000X;
     public Transform endPos;
     public bool inEndZone = false;
     public static EndZone instance;
@@ -32,47 +32,47 @@ public class EndZone : MonoBehaviour
     // Check the position of the car and apply the appropriate speed increase and multiplier
     public void CheckCarPosition(Transform car)
     {
-        if (car.position.z > point10X.position.z && car.position.z < point50X.position.z)
+        if (car.position.z > point5X.position.z && car.position.z < point10X.position.z)
         {
             car.GetComponent<CarScript>().SetSpeedIncrease(-25f);
             inEndZone = true;
-            multiplier = 10f;
+            multiplier = 5f;
         }
-        else if (car.position.z > point50X.position.z && car.position.z < point200X.position.z)
+        else if (car.position.z > point10X.position.z && car.position.z < point25X.position.z)
         {
             car.GetComponent<CarScript>().SetSpeedIncrease(-50f);
             inEndZone = true;
-            multiplier = 50f;
+            multiplier = 10f;
         }
-        else if (car.position.z > point200X.position.z && car.position.z < point500X.position.z)
+        else if (car.position.z > point25X.position.z && car.position.z < point50X.position.z)
         {
             car.GetComponent<CarScript>().SetSpeedIncrease(-75f);
             inEndZone = true;
-            multiplier = 200f;
+            multiplier = 25f;
         }
-        else if (car.position.z > point500X.position.z && car.position.z < point700X.position.z)
+        else if (car.position.z > point50X.position.z && car.position.z < point100X.position.z)
         {
             car.GetComponent<CarScript>().SetSpeedIncrease(-100f);
             inEndZone = true;
-            multiplier = 500f;
+            multiplier = 50f;
         }
-        else if (car.position.z > point700X.position.z && car.position.z < point1000X.position.z)
+        else if (car.position.z > point100X.position.z && car.position.z < point200X.position.z)
         {
             car.GetComponent<CarScript>().SetSpeedIncrease(-125f);
             inEndZone = true;
-            multiplier = 700f;
+            multiplier = 100f;
         }
-        else if (car.position.z > point1000X.position.z && car.position.z < endPos.position.z)
+        else if (car.position.z > point200X.position.z && car.position.z < endPos.position.z)
         {
             car.GetComponent<CarScript>().SetSpeedIncrease(-150f);
             inEndZone = true;
-            multiplier = 1000f;
+            multiplier = 200f;
         }
         else if (car.position.z > endPos.position.z)
         {
             car.GetComponent<CarScript>().SetSpeedIncrease(-99999f);
             inEndZone = true;
-            multiplier = 1000f;
+            multiplier = 200f;
         }
     }
 }
